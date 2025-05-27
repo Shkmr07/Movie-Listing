08c958ceaa23425868c08723666c9ac019c114e8
Ayush Kumar Singh <shkmrsingh@gmail.com>
2025-05-27 23:34:31 +0530

    some change

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const connectDB = require("./config");
 const router = require("./src/routes/combine.routes");
+const cors = require("cors")
 require("dotenv").config();
 
 const app = express();
 
 const port = process.env.PORT;
+
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("Welcome to Movie Listing app");
